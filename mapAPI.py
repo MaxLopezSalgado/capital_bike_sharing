f44ada91bf9308e6ff7fec239bbbeff2a6099df1
MaxLopezSalgado <maxlopezsalgado@gmail.com>
2023-05-11 16:57:00 +0200

    get a list with the  lat and lon values and prepared it for exporting

--- mapAPI.py
+++ mapAPI.py
@@ -1,5 +1,6 @@
+from pprint import pprint # pip install pprintpp
 import googlemaps # pip install googlemaps
-import json
+import json 
 import requests
 
 # save the key in a variable
@@ -8,30 +9,17 @@ API_KEY = 'AIzaSyDOorAGDAn6noddz7AjQzwx3xFrgeHtcV0'
 # create a client
 map_client = googlemaps.Client(key=API_KEY)
 
-# create an empty list to store the station addresses
-station_addresses = []
-
 # get the station information
 station_information = requests.get('https://gbfs.capitalbikeshare.com/gbfs/en/station_information.json').json()
 
-# loop through the list of stations and append the address to the list
-for station in station_information['data']['stations']:
-    station_addresses.append(station['address'])
+# get the station coordinates from the station information and save them in a list of strings
+station_coordinates = [str(station['name']) + ',' + str(station['lat']) + ',' + str(station['lon']) for station in station_information['data']['stations']]
+# print(station_coordinates)
 
-# get the latitude and longitude of the first station
-response = map_client.geocode(station_addresses[0])
-print(response)
-print(response[0]['geometry']['location']['lat'])
-print(response[0]['geometry']['location']['lng'])
+# get the unique values from the list of strings
+unique_station_coordinates = list(set(station_coordinates))
+# print(unique_station_coordinates)
 
-# get the latitude and longitude of the last station
-response = map_client.geocode(station_addresses[-1])
-print(response)
-print(response[0]['geometry']['location']['lat'])
-print(response[0]['geometry']['location']['lng'])
-
-# get the directions from the first station to the last station
-response = map_client.directions(station_addresses[0], station_addresses[-1])
-
-# print the directions
-print(json.dumps(response, indent=4, sort_keys=True))
+# eliminate the duplicates from the list of strings
+unique_station_coordinates = list(dict.fromkeys(station_coordinates))
+print(unique_station_coordinates)
